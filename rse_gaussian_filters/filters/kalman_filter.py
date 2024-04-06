50bf48dffd2c840c0547b39d4fc083c3f764eada
Carlos Argueta <carlos.argueta@lungteh.com>
2024-04-06 09:51:14 +0800

    Added license text to source code

--- rse_gaussian_filters/filters/kalman_filter.py
+++ rse_gaussian_filters/filters/kalman_filter.py
@@ -1,3 +1,8 @@
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+
 import numpy as np 
 
 from ..motion_models import linear_velocity_motion_model
